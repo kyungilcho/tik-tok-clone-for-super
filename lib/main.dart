9b5914b70330cfc41d1a0d6609fa30839b318eb9
kyungil <kyungilcho@medibloc.org>
2026-03-16 15:02:27 +0900

    chore: lock app orientation to portrait up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const App());
 }
